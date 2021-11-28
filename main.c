800c7fb15508b4ea7cc64046f3d08ffec1062a94
Mohnish Kalia <mohnish.kalia@gmail.com>
2021-11-28 01:48:46 -0600

    Only main deck monsters

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
-#include <sqlite3.h>
+#include "sqlite3.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+enum Type
+{
+    Monster = 0x1,
+    Fusion = 0x40,
+    Synchro = 0x2000,
+    Xyz = 0x800000,
+    Link = 0x4000000,
+    ExtraDeck = Fusion | Synchro | Xyz | Link,
+};
 
 typedef struct
 {
     char *id;
     char *name;
-    char *type;
+    char *race;
     char *attribute;
     char *level;
     char *atk;
@@ -18,7 +28,7 @@ void printCardInfo(FILE *sink, CardInfo *ci)
     fprintf(sink, "\n");
     fprintf(sink, "id = %s\n", ci->id);
     fprintf(sink, "name = %s\n", ci->name);
-    fprintf(sink, "type = %s\n", ci->type);
+    fprintf(sink, "race = %s\n", ci->race);
     fprintf(sink, "attribute = %s\n", ci->attribute);
     fprintf(sink, "level = %s\n", ci->level);
     fprintf(sink, "atk = %s\n", ci->atk);
@@ -28,16 +38,21 @@ void printCardInfo(FILE *sink, CardInfo *ci)
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName)
 {
-    CardInfo ci = {
-        .id = argv[0],
-        .name = argv[1],
-        .type = argv[2],
-        .attribute = argv[3],
-        .level = argv[4],
-        .atk = argv[5],
-        .def = argv[6],
-    };
-    printCardInfo(stdout, &ci);
+    char *ptr;
+    long type = strtol(argv[2], &ptr, 10);
+    if ((type & Monster) != 0 && (type & ExtraDeck) == 0)
+    {
+        CardInfo ci = {
+            .id = argv[0],
+            .name = argv[1],
+            .race = argv[3],
+            .attribute = argv[4],
+            .level = argv[5],
+            .atk = argv[6],
+            .def = argv[7],
+        };
+        printCardInfo(stdout, &ci);
+    }
     return 0;
 }
 
@@ -63,8 +78,8 @@ int main(void)
         return (1);
     }
 
-    char *statement = "select d.id, name, type, attribute, level, atk, def from datas d inner join texts t on d.id = t.id";
-    FILE* out = fopen("cards.txt", "w");
+    char *statement = "select d.id, name, type, race, attribute, level, atk, def from datas d inner join texts t on d.id = t.id";
+    FILE *out = fopen("cards.txt", "w");
     stdout = out;
     rc = sqlite3_exec(db, statement, callback, 0, &zErrMsg);
     fclose(out);
